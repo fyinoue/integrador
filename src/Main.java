69b9d42a31c8263a1afb47123d656896460cefba
1714290044 <1714290044@SJB8-D18.academico.iesb.br>
2018-04-25 19:26:25 -0300

    Teste

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         JFrame janela = new JFrame();
+        JButton botao = new JButton();
+        janela.add(botao);
         janela.setSize(640,480);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
+        
     }
 }
