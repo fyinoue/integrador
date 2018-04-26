3dfc24a02312c32f5cc937ecba74e29b5d0aa957
ryosh <inouefy@gmail.com>
2018-04-26 20:55:25 -0300

    Adicionei botao ao JPanel

--- src/TelaFelipe.java
+++ src/TelaFelipe.java
@@ -1,10 +1,14 @@
 
 import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class TelaFelipe extends JPanel {
     public TelaFelipe() {
-        this.setBackground(Color.red);
+        JButton botao = new JButton("Botao");
+        
+        this.add(botao);
+        this.setBackground(Color.BLACK);
         this.setVisible(false);
     }
 }
