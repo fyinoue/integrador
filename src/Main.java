723741d9226f715d015da26fc078026cea4be189
ryosh <inouefy@gmail.com>
2018-04-26 11:34:26 -0300

    Adicionado Painel na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
+        JPanel painel = new JPanel();
+        painel.setBackground(Color.CYAN);
+        
         JFrame janela = new JFrame();
+        janela.add(painel);
         janela.setSize(640,480);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
