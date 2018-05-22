ced09b713917739a58e99731ae1fb04c64332afc
1714290044 <1714290044@SJB8-D18.academico.iesb.br>
2018-05-21 21:38:13 -0300

    CARDLAYOUT OK!!!!!!!!!!!!!!top

--- src/TelaFelipe.java
+++ src/TelaFelipe.java
@@ -1,14 +1,16 @@
 
 import java.awt.Color;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class TelaFelipe extends JPanel {
     public TelaFelipe() {
         JButton botao = new JButton("Botao");
-        
+        this.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+
         this.add(botao);
-        this.setBackground(Color.BLACK);
-        this.setVisible(false);
+        this.setBackground(Color.WHITE);
+        
     }
 }
