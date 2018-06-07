6096faffdcfb572e087f3993f0887f9d7a14f413
1714290011 <1714290011@SJB8-D16.academico.iesb.br>
2018-06-06 21:15:10 -0300

    Tela Pivante

--- test/Pivante/TelaPivante.java
+++ test/Pivante/TelaPivante.java
@@ -1,79 +1,72 @@
+
+package Pivante;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
 
 public class TelaPivante extends JPanel{
-    private JLabel textoLogin;
-    private JLabel textoSenha;
-    private JLabel textoTitulo;
-    private JTextField campoLogin;
-    private JPasswordField campoSenha;
-    private JButton botaoEntrar;
-
-    public TelaPivante() {
-        textoLogin = new JLabel("Login:   ");
-        textoSenha = new JLabel("Senha: ");
-        textoTitulo = new JLabel("Entrar no Sistema");
-
-        textoTitulo.setFont(new java.awt.Font("Tahoma", 1, 14));
-
-        campoLogin = new JTextField(20);
-        campoSenha = new JPasswordField(20);
-
-        botaoEntrar = new JButton("Entrar");
-        botaoEntrar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Login efetuado com sucesso!");
-            }
-        });
-
-        JPanel painelLogin = new JPanel();
-        painelLogin.add(textoLogin);
-        painelLogin.add(campoLogin);
-
-        JPanel painelSenha = new JPanel();
-        painelSenha.add(textoSenha);
-        painelSenha.add(campoSenha);
-
-        JPanel painelTitulo = new JPanel();
-        painelTitulo.add(textoTitulo);
-
+    private JLabel txTipo;
+    private JLabel txModelo;
+    private JComboBox Tipo;
+    private JComboBox Modelo;
+    private JButton Voltar;
+    
+    public TelaPivante(){
+        String[] tipos = {"Confidencial", "Restrito", "Livre"};
+        String[] modelos = {"Ato", "Ata"};
+        
+        txTipo = new JLabel("Tipo: ");
+        txModelo = new JLabel("Modelo: ");
+        
+        Tipo = new JComboBox(tipos);
+        Modelo = new JComboBox(modelos);
+        
+        Voltar = new JButton("Voltar");
+        
+        Tipo.setSelectedIndex(Tipo.getItemCount()-1);
+        
+        add(Tipo);
+        add(Modelo);
+        
+        JPanel painelCombo1 = new JPanel();
+        painelCombo1.add(txTipo);
+        painelCombo1.add(Tipo);
+        
+        JPanel painelCombo2 = new JPanel();
+        painelCombo2.add(txModelo);
+        painelCombo2.add(Modelo);
+        
         JPanel painelBotao = new JPanel();
-        painelBotao.add(botaoEntrar);
-
+        painelBotao.add(Voltar);
+        
         GridLayout layout = new GridLayout(10, 1);
-
+        
         JPanel painelEspaço = new JPanel();
-        JPanel painelEspaço2 = new JPanel();
-
+        
         JPanel painelCentral = new JPanel();
         painelCentral.setLayout(layout);
-        painelCentral.add(painelEspaço2);
         painelCentral.add(painelEspaço);
-        painelCentral.add(painelTitulo);
-        painelCentral.add(painelLogin);
-        painelCentral.add(painelSenha);
+        painelCentral.add(painelCombo1);
+        painelCentral.add(painelCombo2);
         painelCentral.add(painelBotao);
         
-        this.setBorder(BorderFactory.createLineBorder(Color.PINK));
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         BorderLayout layout2 = new BorderLayout();
         this.setLayout(layout2);
         this.add(painelCentral);
         
-        
-        
-        
     }
+    
+    
+   
+    
+    
     
 }
